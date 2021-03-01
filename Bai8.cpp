c1e2f3506bdd846574e3b4e76d9d7fe982de25ca
thuyduong7 <78083024+thuyduong7@users.noreply.github.com>
2021-03-01 20:56:48 +0700

    Create Bai8.cpp

---
+++ Bai8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/**int rnd(double n)
+{
+    int m;
+    if (ceil(n) - n <= 0.5) m = ceil(n);
+    else m = floor(n);
+    return m;
+}
+*/
+
+int rnd(double n)
+{
+    int m;
+    
+    return m;
+}
+
+int main(){
+    double n;
+    cin >> n;
+    cout << rnd(n);
+    return 0;
+}
